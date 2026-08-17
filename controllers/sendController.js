e470f938464ca49e501717f776682a41ad631e1d
lymyr <176290600+lymyr@users.noreply.github.com>
2026-08-17 20:26:30 +0800

    feat: added message validation

--- controllers/sendController.js
+++ controllers/sendController.js
@@ -1,15 +1,31 @@
+import { body, validationResult } from "express-validator"
 import { 
     postMessage as queryAddMsg
  } from "../db/query.js"
+
+const messageValidator = [
+    body('username')
+        .trim().notEmpty().withMessage("Username must not be empty.")
+        .isLength({max: 20}).withMessage("Username must not exceed 20 characters"),
+    body('message')
+        .trim().notEmpty().withMessage("Message must not be empty.")
+        .isLength({max:300}).withMessage("Message should not exceed 300 characters.")
+]
 
 function getSendPage(req, res) {
     res.render('send')
 }
 
-async function postMessage(req, res) {
-    const { username, message } = req.body
-    await queryAddMsg(username, message, new Date())
-    res.redirect('/')
-}
+const postMessage = [
+    messageValidator,
+    async (req, res) => {
+        const errors = validationResult(req)
+        if (errors.isEmpty()) {
+            await queryAddMsg(req.body.username, req.body.message, new Date())
+            return res.redirect('/')
+        }
+        res.status(400).render('send', {errors: errors.mapped()})
+    }
+]
 
 export { getSendPage, postMessage }
