b51200f97f814335d02087f31e1bc34a0230b230
lymyr <176290600+lymyr@users.noreply.github.com>
2026-08-17 19:59:59 +0800

    feat: added psql to adding messages

--- controllers/sendController.js
+++ controllers/sendController.js
@@ -1,15 +1,14 @@
-import messages from "../messages.js"
+import { 
+    postMessage as queryAddMsg
+ } from "../db/query.js"
 
 function getSendPage(req, res) {
     res.render('send')
 }
 
-function postMessage(req, res) {
-    messages.push({
-        user: req.body.username,
-        text: req.body.message,
-        added: new Date()
-    })
+async function postMessage(req, res) {
+    const { username, message } = req.body
+    await queryAddMsg(username, message, new Date())
     res.redirect('/')
 }
 
